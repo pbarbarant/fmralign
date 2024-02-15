0a729131c5a63df95917607cfcfe527c6f08091b
Pierre-Louis Barbarant <plbarbarant@gmail.com>
2024-02-15 10:44:19 +0100

    Add fmralign.fugw module for functional alignment

---
+++ fmralign/fugw.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+""" 
+The :mod:`fmralign.fugw` module includes a set of tools to align fMRI data
+using the functional alignment algorithm proposed by [FUGW]_.
+
+References
+----------
+.. [FUGW] Thual, Alexis and Tran, Huy and Zemskova, Tatiana and Courty, Nicolas 
+    and Flamary, Rémi and Dehaene, Stanislas and Thirion, Bertrand (2022).
+    Aligning individual brains with fused unbalanced Gromov Wasserstein.
+    Advances in Neural Information Processing Systems, 35, 21792--21804.
+    https://doi.org/10.48550/ARXIV.2206.09398
+    
+    
+"""
+# Author: Pierre-Louis Barbarant
+# License: simplified BSD
+
