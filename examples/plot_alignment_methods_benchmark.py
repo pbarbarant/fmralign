1f87a7686e50f86c1b3eb5fde85e8e33cf5e2f24
Pierre-Louis Barbarant <plbarbarant@mail.com>
2025-07-07 15:09:24 +0200

    Use ward in plot_alignment

--- examples/plot_alignment_methods_benchmark.py
+++ examples/plot_alignment_methods_benchmark.py
@@ -141,7 +141,10 @@ methods = ["identity", "scaled_orthogonal", "ridge_cv", "optimal_transport"]
 
 for method in methods:
     alignment_estimator = PairwiseAlignment(
-        alignment_method=method, n_pieces=n_pieces, masker=roi_masker
+        alignment_method=method,
+        clustering="ward",
+        n_pieces=n_pieces,
+        masker=roi_masker,
     )
     alignment_estimator.fit(source_train, target_train)
     target_pred = alignment_estimator.transform(source_test)
